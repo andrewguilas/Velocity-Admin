78a694495c66d31cc2799348f64ddbb0bdad997b
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-01 17:13:22 -0400

    Added more keybinds for auto complete
    - Open (backslash)
    - Exit (backslash, esc, return)
    - Use selected field (return, tab)
    - New field (up, down)

--- src/StarterPlayer/StarterPlayerScripts/FastAdmin/Settings.lua
+++ src/StarterPlayer/StarterPlayerScripts/FastAdmin/Settings.lua
@@ -2,37 +2,25 @@ local Settings = {
     CommandBar = {
         Startup = {
             Key = Enum.KeyCode.BackSlash,
-            HiddenPos = UDim2.new(0.3, 10, -0.2, 10),
+            Exit = Enum.KeyCode.Escape,
+            HiddenPos = UDim2.new(0.3, 10, -1, 10),
             ShownPos = UDim2.new(0.3, 10, 0, 10),
-            TweenInfo = {
-                Direction = Enum.EasingDirection.In,
-                Style = Enum.EasingStyle.Linear,
-                Duration = 0.2,
-                Overrides = true
-            },
         },
         Text = {
             Size = UDim2.new(0, 40, 0, 20),
             MaxSize = 500,
-            TweenInfo = {
-                Direction = Enum.EasingDirection.In,
-                Style = Enum.EasingStyle.Linear,
-                Duration = 0.2,
-                Overrides = true
-            }
         },
         AutoComplete = {
-            Key = Enum.KeyCode.Tab,
+            Key1 = Enum.KeyCode.Tab,
+            Key2 = Enum.KeyCode.Return,
+            UpKey = Enum.KeyCode.Up,
+            DownKey = Enum.KeyCode.Down,
+            SelectedColor = Color3.fromRGB(100, 100, 100),
+            NonSelectedColor = Color3.fromRGB(0, 0, 0),
             ShowPos = UDim2.new(0.3, 10, 0, 32),
             FieldSize = UDim2.new(20, 0, 0, 20),
             FieldSpacing = 80,
             ArgSplit = " ",
-            TweenInfo = {
-                Direction = Enum.EasingDirection.In,
-                Style = Enum.EasingStyle.Linear,
-                Duration = 0.2,
-                Overrides = true
-            },
             Words = {
                 ["Jet"] = "was good, then bad, then tried to be good, but was actually bad the ENTIRE TIME, and then he became bad  because he was brain washed, but then he realized that he was brain washed so he became good, but then he was controlled again from the previous brain washing experience and became bad, but when he died, he becam,e good.",
                 ["Potato"] = "Makes french fries!",
--- src/StarterPlayer/StarterPlayerScripts/FastAdmin/init.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/FastAdmin/init.client.lua
@@ -57,7 +57,7 @@ end
 
 function RunAutoComplete()
 
-    for _,Field in pairs(Core.Get(AutoComplete, "Frame")) do
+    for _,Field in pairs(Core.Get(AutoComplete, "TextButton")) do
         Field:Destroy()
     end
 
@@ -65,11 +65,17 @@ function RunAutoComplete()
     local BiggestSize = 0
     if PossibleFields then
 
-        for i,Field in pairs(PossibleFields) do
+        for i, Field in pairs(PossibleFields) do
             local NewField = AutoComplete.ListLayout.Template:Clone()
             NewField.Title.Text = Field.Title
             NewField.Description.Text = Field.Description
             NewField.Name, NewField.LayoutOrder = i, i
+
+            if i == 1 then
+                NewField.IsSelected.Value = true
+                NewField.BackgroundColor3 = Settings.CommandBar.AutoComplete.SelectedColor
+            end
+
             NewField.Parent = AutoComplete
             NewField.Title.Size = UDim2.new(0, NewField.Title.TextBounds.X, 1, 0)
 
@@ -88,62 +94,110 @@ function RunAutoComplete()
             if GoalX > BiggestSize then
                 BiggestSize = GoalX
             end
+
+            NewField.MouseButton1Click:Connect(function()
+                AutoCorrect(NewField)
+            end)
+
         end
 
-        for _,Field in pairs(Core.Get(AutoComplete, "Frame")) do
+        for _,Field in pairs(Core.Get(AutoComplete, "TextButton")) do
             Field.Size = UDim2.new(0, BiggestSize, Field.Size.Y.Scale, 0)
         end
 
-        local Goal = Settings.CommandBar.AutoComplete.FieldSize + UDim2.new(0, AutoComplete.ListLayout.AbsoluteContentSize.X, 0, 0)
-        AutoComplete:TweenSize(Goal, Settings.CommandBar.AutoComplete.TweenInfo.Direction, Settings.CommandBar.AutoComplete.TweenInfo.Style, Settings.CommandBar.AutoComplete.TweenInfo.Duration, Settings.CommandBar.AutoComplete.TweenInfo.Overrides)
-
         AutoComplete.Shown.Value = false
-        AutoComplete:TweenPosition(Settings.CommandBar.AutoComplete.ShowPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)
+        AutoComplete.Position = Settings.CommandBar.AutoComplete.ShowPos
+        AutoComplete.Size = Settings.CommandBar.AutoComplete.FieldSize + UDim2.new(0, AutoComplete.ListLayout.AbsoluteContentSize.X, 0, 0)
     else
         AutoComplete.Shown.Value = false
-        AutoComplete:TweenPosition(Settings.CommandBar.Startup.HiddenPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)      
+        AutoComplete.Position = Settings.CommandBar.Startup.HiddenPos
     end
 end
 
 function ExpandUI()
-    local Goal = Settings.CommandBar.Text.Size + UDim2.new(0, TextBox.TextBounds.X, 0, 0)
-    CommandBar:TweenSize(Goal, Settings.CommandBar.Text.TweenInfo.Direction, Settings.CommandBar.Text.TweenInfo.Style, Settings.CommandBar.Text.TweenInfo.Duration, Settings.CommandBar.Text.TweenInfo.Overrides)
+    CommandBar.Size = Settings.CommandBar.Text.Size + UDim2.new(0, TextBox.TextBounds.X, 0, 0)
+end
+
+function AutoCorrect(SelectedField)
+    local Args = string.split(TextBox.Text, Settings.CommandBar.AutoComplete.ArgSplit)
+    table.remove(Args, #Args)
+    table.insert(Args, SelectedField.Title.Text)
+
+    TextBox.CursorPosition = 1
+    RunService.RenderStepped:Wait()
+    TextBox.Text = table.concat(Args, Settings.CommandBar.AutoComplete.ArgSplit) .. " "
+    TextBox.CursorPosition = #TextBox.Text + 1
+end
+
+function CloseUI()
+    CommandBar.Shown.Value = false
+    CommandBar.Position = Settings.CommandBar.Startup.HiddenPos
+
+    AutoComplete.Shown.Value = false
+    AutoComplete.Position = Settings.CommandBar.Startup.HiddenPos
+    
+    TextBox:ReleaseFocus()
+    DisconnectCon("CloseUI")
 end
 
 function OpenUI(Input, GameProcessed)
-    if Input.KeyCode == Settings.CommandBar.Startup.Key then
-        if CommandBar.Shown.Value then
-            CommandBar.Shown.Value = false
-            CommandBar:TweenPosition(Settings.CommandBar.Startup.HiddenPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)
-
-            AutoComplete.Shown.Value = false
-            AutoComplete:TweenPosition(Settings.CommandBar.Startup.HiddenPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)
-            
-            TextBox:ReleaseFocus()
-            DisconnectCon("CloseUI")
-        else
+    if Input.KeyCode == Settings.CommandBar.Startup.Key or Input.KeyCode == Settings.CommandBar.Startup.Exit then
+        if not CommandBar.Shown.Value then
             CommandBar.Shown.Value = true
-            CommandBar:TweenPosition(Settings.CommandBar.Startup.ShownPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)
+            CommandBar.Position = Settings.CommandBar.Startup.ShownPos
 
             AutoComplete.Shown.Value = false
-            CommandBar:TweenPosition(Settings.CommandBar.Startup.ShownPos, Settings.CommandBar.Startup.TweenInfo.Direction, Settings.CommandBar.Startup.TweenInfo.Style, Settings.CommandBar.Startup.TweenInfo.Duration, Settings.CommandBar.Startup.TweenInfo.Overrides)
+            CommandBar.Position = Settings.CommandBar.Startup.ShownPos
             
             TextBox:CaptureFocus()
-            wait()
+            RunService.RenderStepped:Wait()
             TextBox.Text = ""
 
             DisconnectCon("CloseUI")
             Cons.CloseUI = TextBox.InputBegan:Connect(OpenUI)
+        else
+            CloseUI()
         end  
-    elseif Input.KeyCode == Settings.CommandBar.AutoComplete.Key and AutoComplete:FindFirstChild("1") then
-        local Args = string.split(TextBox.Text, Settings.CommandBar.AutoComplete.ArgSplit)
-        table.remove(Args, #Args)
-        table.insert(Args, AutoComplete[1].Title.Text)
+    elseif Input.KeyCode == Settings.CommandBar.AutoComplete.Key1 or Input.KeyCode == Settings.CommandBar.AutoComplete.Key2 then
+        local SelectedField
+        for _,Field in pairs(Core.Get(AutoComplete, "TextButton")) do
+            if Field.IsSelected.Value then
+                SelectedField = Field
+            end
+        end
 
-        TextBox.CursorPosition = 1
-        RunService.RenderStepped:Wait()
-        TextBox.Text = table.concat(Args, Settings.CommandBar.AutoComplete.ArgSplit) .. " "
-        TextBox.CursorPosition = #TextBox.Text + 1
+        if SelectedField then
+            AutoCorrect(SelectedField)
+        else
+            CloseUI()
+        end
+    elseif Input.KeyCode == Settings.CommandBar.AutoComplete.UpKey or Input.KeyCode == Settings.CommandBar.AutoComplete.DownKey then
+
+        local OldSelectedField
+        for _,Field in pairs(Core.Get(AutoComplete, "TextButton")) do
+            if Field.IsSelected.Value then
+                OldSelectedField = Field
+                break
+            end
+        end
+
+        local Step
+        if Input.KeyCode == Settings.CommandBar.AutoComplete.UpKey then
+            Step = -1
+        else
+            Step = 1
+        end 
+
+        if OldSelectedField then
+            local NewSelectedField = AutoComplete:FindFirstChild(OldSelectedField.Name + Step)
+            if NewSelectedField then
+                OldSelectedField.BackgroundColor3 = Settings.CommandBar.AutoComplete.NonSelectedColor
+                OldSelectedField.IsSelected.Value = false
+            
+                NewSelectedField.BackgroundColor3 = Settings.CommandBar.AutoComplete.SelectedColor
+                NewSelectedField.IsSelected.Value = true
+            end
+        end
     end   
 end
 
