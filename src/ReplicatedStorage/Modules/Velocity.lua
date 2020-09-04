ae7ba43ab65e95e8ddf805a8776eac88551be3d6
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-04 19:57:48 -0400

    Added char cmd

--- src/ReplicatedStorage/Modules/Velocity.lua
+++ src/ReplicatedStorage/Modules/Velocity.lua
@@ -35,6 +35,82 @@ end
 
 -- // Humanoid \\ --
 
+Velocity.Commands.char = {
+    ["Description"] = "Changes player 1's appearance to any user on Roblox: player 2.",
+    ["Arguments"] = {
+        [1] = {
+            ["Title"] = "player1",
+            ["Description"] = "The player which will have their character changed.",
+            ["Choices"] = function()
+                local Players = {}
+                for _,p in pairs(game.Players:GetPlayers()) do
+                    table.insert(Players, p.Name)
+                end
+                return Players
+            end
+        },
+        [2] = {
+            ["Title"] = "player2",
+            ["Description"] = "What player1 will be turned into.",
+            ["Choices"] = true,
+        }
+    },
+    ["Run"] = function(CurrentPlayer, Player, Player2)
+
+        -- Check if necessary arguments are there
+        if not Player then
+            return false, "Player 1 Argument Missing"
+        elseif not Player2 then
+            return false, "Player 2 Argument Missing"
+        end
+
+        -- Get target name & id
+        local Player2Name, Player2Id
+
+        if tonumber(Player2) then
+            Player2Id = Player2
+            
+            local success = pcall(function()
+                Player2Name = game.Players:GetNameFromUserIdAsync(Player2Id)
+            end)
+
+            if not success then
+                return false, "Error finding username of " .. Player2Id
+            end      
+        elseif tostring(Player2) then
+            Player2Name = Player2
+            
+            local success = pcall(function()
+                Player2Id = game.Players:GetUserIdFromNameAsync(Player2Name)
+            end)
+
+            if not success then
+                return false, "Error finding user ID of " .. Player2Name
+            end
+        else
+            return false, "Player2 is not a valid argument type."      
+        end
+
+        -- Run Command
+        local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+        if Players then
+            local Info = {}
+            for _,p in pairs(Players) do
+                p.CharacterAppearanceId = Player2Id
+                p:LoadCharacter()
+                table.insert(Info, {
+                    Success = true,
+                    Status = p.Name .. "'s character was changed to " .. Player2Name .. " (UserId: " .. Player2Id ..")"
+                })
+            end   
+            return Info           
+        else
+            return false, Player .. " is not a valid player."
+        end
+
+    end
+}
+
 Velocity.Commands.speed = {
     ["Description"] = "Changes a player's walk speed.",
     ["Arguments"] = {
