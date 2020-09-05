8797a2fe8351f790412b8a02d1cb6971b0f9c599
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-05 15:04:48 -0400

    added god & ungod cmd

--- src/ReplicatedStorage/Modules/Velocity.lua
+++ src/ReplicatedStorage/Modules/Velocity.lua
@@ -1046,6 +1046,103 @@ Velocity.Commands.heal = {
     end
 }
 
+Velocity.Commands.god = {
+    ["Description"] = "Sets the player's health to infinite.",
+    ["Arguments"] = {
+        [1] = {
+            ["Title"] = "player",
+            ["Description"] = "The player you want to god.",
+            ["Choices"] = function()
+                local Players = {}
+                for _,p in pairs(game.Players:GetPlayers()) do
+                    table.insert(Players, p.Name)
+                end
+                return Players
+            end
+        },
+    },
+    ["Run"] = function(CurrentPlayer, Player)
+
+        -- Check if necessary arguments are there
+        if not Player then
+            return false, "Player Argument Missing"
+        end
+
+        -- Run Command
+        local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+        if Players then
+            for _,p in pairs(Players) do
+                local Char = p.Character
+                if Char then
+                    local Hum = Char:WaitForChild("Humanoid")
+                    if not Velocity.TempData[CurrentPlayer.Name].God then                      
+                        Velocity.TempData[CurrentPlayer.Name].God = {
+                            Health = Hum.Health,
+                            MaxHealth = Hum.MaxHealth
+                        }
+                        Hum.MaxHealth, Hum.Health = math.huge, math.huge
+                        return true, Player .. " was godded."
+                    else
+                        return false, Player .. " is already godded."
+                    end
+                else
+                    return false, Player .. "'s character does not exist."
+                end   
+            end              
+        else
+            return false, Player .. " is not a valid player."
+        end
+
+    end
+}
+
+Velocity.Commands.ungod = {
+    ["Description"] = "Sets the player's health to infinite.",
+    ["Arguments"] = {
+        [1] = {
+            ["Title"] = "player",
+            ["Description"] = "The player you want to god.",
+            ["Choices"] = function()
+                local Players = {}
+                for _,p in pairs(game.Players:GetPlayers()) do
+                    table.insert(Players, p.Name)
+                end
+                return Players
+            end
+        },
+    },
+    ["Run"] = function(CurrentPlayer, Player)
+
+        -- Check if necessary arguments are there
+        if not Player then
+            return false, "Player Argument Missing"
+        end
+
+        -- Run Command
+        local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+        if Players then
+            for _,p in pairs(Players) do
+                local Char = p.Character
+                if Char then
+                    if Velocity.TempData[CurrentPlayer.Name].God then
+                        local Hum = Char:WaitForChild("Humanoid")
+                        Hum.MaxHealth, Hum.Health = Velocity.TempData[CurrentPlayer.Name].God.MaxHealth, Velocity.TempData[CurrentPlayer.Name].God.Health
+                        Velocity.TempData[CurrentPlayer.Name].God = nil
+                        return true, Player .. " was ungodded."
+                    else
+                        return false, Player .. " is already ungodded."
+                    end
+                else
+                    return false, Player .. "'s character does not exist."
+                end   
+            end              
+        else
+            return false, Player .. " is not a valid player."
+        end
+
+    end
+}
+
 Velocity.Commands.health = {
     ["Description"] = "Changes a player's health.",
     ["Arguments"] = {
