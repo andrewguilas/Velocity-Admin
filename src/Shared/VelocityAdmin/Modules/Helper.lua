94456f7fd726e27b7365a5c9a9c84cc2bb5526c8
advanceinnovations <68887636+advanceinnovations@users.noreply.github.com>
2020-09-09 22:44:33 -0400

    Created random and team: args

--- src/Shared/VelocityAdmin/Modules/Helper.lua
+++ src/Shared/VelocityAdmin/Modules/Helper.lua
@@ -21,6 +21,24 @@ function Helper.FindPlayer(Key, p)
         Core.TableRemove(Players, p)
     elseif Key == "me" then
         Players = {p}
+    elseif Key == "random" then
+        Players = game.Players:GetPlayers()
+        if Players[1] ~= nil then
+            local value = math.random(1,#Players)
+            local picked = Players[value]
+        end
+    elseif Key:find("team:") then
+        Key = Key:sub(5,#Key):lower():gsub("%s+", "")
+
+        for _,v in pairs(game:GetService("Teams")) do
+            if v.Name:lower():gsub("%s+", "") == Key then
+                for _,player in pairs(game.Players:GetPlayers()) do
+                    if player.Team.Name == Key then
+                        table.insert(Players,player)
+                    end
+                end
+            end
+        end
     else
         for _,p in pairs(game.Players:GetPlayers()) do
             if p.Name:lower() == Key:lower() then
