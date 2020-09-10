135edd7ba2ca28c3de554e86ffa001b0b8d770b4
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-10 10:22:07 -0400

    fixed speed cmd

--- src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/speed.lua
+++ src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/speed.lua
@@ -1,15 +1,14 @@
 local Cmd = {}
 local Helper = require(game.ReplicatedStorage.VelocityAdmin.Modules.Helper)
-local Chat = game:GetService("Chat")
 
 ----------------------------------------------------------------------
 
-Cmd.Description = "Changes a player's walk speed."
+Cmd.Description = "Changes the player's walkspeed."
 
 Cmd.Arguments = {
     [1] = {
         ["Title"] = "player",
-        ["Description"] = "The player you want to change the walk speed of..",
+        ["Description"] = "The player you want to change the speed of",
         ["Choices"] = function()
             local Players = {}
             for _,p in pairs(game.Players:GetPlayers()) do
@@ -20,7 +19,7 @@ Cmd.Arguments = {
     },
     [2] = {
         ["Title"] = "amount",
-        ["Description"] = "The amount you want to set the player's walkspeed to.",
+        ["Description"] = "The amount you want to set the player's walkspeed to",
         ["Choices"] = true,
     }
 }
@@ -35,18 +34,27 @@ Cmd.Run = function(CurrentPlayer, Player, Amount)
     end
 
     -- Run Command
-    local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+    local Players = Helper.FindPlayer(Player, CurrentPlayer)
     if Players then
+        local Info = {}
         for _,p in pairs(Players) do
             local Char = p.Character
             if Char then
                 local Hum = Char:WaitForChild("Humanoid")
                 Hum.WalkSpeed = Amount
-                return true, Player .. "'s walk speed was changed to " .. Amount
+
+                Info:insert({
+                    Success = true,
+                    Status = Player .. "'s walk speed was changed to " .. Amount
+                })
             else
-                return false, Player .. "'s character does not exist."
+                Info:insert({
+                    Success = false,
+                    Status = Player .. "'s character does not exist."
+                })
             end   
-        end              
+        end    
+        return Info          
     else
         return false, Player .. " is not a valid player."
     end
