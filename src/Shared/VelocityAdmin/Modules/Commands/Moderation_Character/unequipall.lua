6d054cecb8425b7049abb4830f822bc6dbb22356
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-10 10:10:13 -0400

    Fixed unequippall cmd

--- src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/unequipall.lua
+++ src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/unequipall.lua
@@ -1,15 +1,14 @@
 local Cmd = {}
 local Helper = require(game.ReplicatedStorage.VelocityAdmin.Modules.Helper)
-local Chat = game:GetService("Chat")
 
 ----------------------------------------------------------------------
 
-Cmd.Description = "Unequipps the tools the player is holding if any."
+Cmd.Description = "Unequipps the tools the player is holding."
 
 Cmd.Arguments = {
     [1] = {
         ["Title"] = "player",
-        ["Description"] = "The player you want to reset the name of.",
+        ["Description"] = "The player you want to remove all the tools of.",
         ["Choices"] = function()
             local Players = {}
             for _,p in pairs(game.Players:GetPlayers()) do
@@ -28,8 +27,9 @@ Cmd.Run = function(CurrentPlayer, Player)
     end
 
     -- Run Command
-    local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+    local Players = Helper.FindPlayer(Player, CurrentPlayer)
     if Players then
+        local Info = {}
         for _,p in pairs(Players) do
             local Char = p.Character
             if Char then
@@ -44,14 +44,24 @@ Cmd.Run = function(CurrentPlayer, Player)
 
                 if #Tools > 0 then
                     Hum:UnequipTools()
-                    return true, Player .. " unequipped... " .. table.concat(Tools, ", ")
+                    Info:insert({
+                        Success = true,
+                        Status = Player .. " unequipped... " .. table.concat(Tools, ", ")
+                    })
                 else
-                    return true, Player .. " has no tools in their backpack."
+                    Info:insert({
+                        Success = true,
+                        Status = Player .. " has no tools in their backpack."
+                    })
                 end
             else
-                return false, p.Name .. "'s character does not exist.."
+                Info:insert({
+                    Success = false,
+                    Status = p.Name .. "'s character does not exist.."
+                })
             end
-        end              
+        end      
+        return Info        
     else
         return false, Player .. " is not a valid player."
     end
