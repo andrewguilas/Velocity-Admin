2b0b71eb6553254da7a2e95bfca63c30b129efac
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-12 11:27:20 -0400

    Fixed ungod cmd

--- src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/ungod.lua
+++ src/Shared/VelocityAdmin/Modules/Commands/Moderation_Character/ungod.lua
@@ -1,15 +1,14 @@
 local Cmd = {}
 local Helper = require(game.ReplicatedStorage.VelocityAdmin.Modules.Helper)
-local Chat = game:GetService("Chat")
 
 ----------------------------------------------------------------------
 
-Cmd.Description = "Sets the player's health to infinite."
+Cmd.Description = "Sets the player's health to the previous health before being goded."
 
 Cmd.Arguments = {
     [1] = {
         ["Title"] = "player",
-        ["Description"] = "The player you want to god.",
+        ["Description"] = "The player you want to ungod (username/user ID)",
         ["Choices"] = function()
             local Players = {}
             for _,p in pairs(game.Players:GetPlayers()) do
@@ -28,23 +27,35 @@ Cmd.Run = function(CurrentPlayer, Player)
     end
 
     -- Run Command
-    local Players = Velocity.Helper.FindPlayer(Player, CurrentPlayer)
+    local Players = Helper.FindPlayer(Player, CurrentPlayer)
     if Players then
+        local Info = {}
         for _,p in pairs(Players) do
             local Char = p.Character
             if Char then
-                if Velocity.TempData[CurrentPlayer.Name].God then
+                if Helper.Data[CurrentPlayer.Name].God then
                     local Hum = Char:WaitForChild("Humanoid")
-                    Hum.MaxHealth, Hum.Health = Velocity.TempData[CurrentPlayer.Name].God.MaxHealth, Velocity.TempData[CurrentPlayer.Name].God.Health
-                    Velocity.TempData[CurrentPlayer.Name].God = nil
-                    return true, Player .. " was ungodded."
+                    Hum.MaxHealth, Hum.Health = Helper.Data[CurrentPlayer.Name].God.MaxHealth, Helper.Data[CurrentPlayer.Name].God.Health
+                    Helper.Data[CurrentPlayer.Name].God = nil
+
+                    Info:insert({
+                        Success = true,
+                        Status = Player .. " was ungodded."
+                    })
                 else
-                    return false, Player .. " is already ungodded."
+                    Info:insert({
+                        Success = true,
+                        Status = Player .. " is already ungodded."
+                    })
                 end
             else
-                return false, Player .. "'s character does not exist."
+                Info:insert({
+                    Success = false,
+                    Status = Player .. "'s character does not exist."
+                })
             end   
-        end              
+        end        
+        return Info      
     else
         return false, Player .. " is not a valid player."
     end
