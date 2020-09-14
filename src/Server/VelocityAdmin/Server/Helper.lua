1daac7596631742657df51cdfee979679b8dba24
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-09-14 10:14:49 -0400

    Made server script modular, started on datastore

--- src/Server/VelocityAdmin/Server/Helper.lua
+++ src/Server/VelocityAdmin/Server/Helper.lua
@@ -1,5 +1,14 @@
 local Module = {}
 
+----------------------------------
+
+-- // Variables \\ --
+
+local DataStoreService = game:GetService("DataStoreService")
+local Settings, SharedHelper, BanStore, Commands
+
+-- // Defaults \\ --
+
 function Module.SendScripts()
     local Velocity = script.Parent.Parent
 
@@ -16,6 +25,77 @@ function Module.SendScripts()
     end
 
     Velocity:Destroy()
+    Settings = require(game.ReplicatedStorage.VelocityAdmin.Modules.Settings)
+    SharedHelper = require(game.ReplicatedStorage.VelocityAdmin.Modules.Helper)
+    BanStore = DataStoreService:GetDataStore(Settings.Basic.BanScope)
+    Commands = game.ReplicatedStorage.VelocityAdmin.Modules.Commands
 end
+
+-- // Bans \\ --
+
+function Module.CheckSlocked(p)
+    local Reason = SharedHelper.Data.ServerLocked
+    if Reason then
+        p:Kick("Server is locked: " .. Reason)
+    end
+end
+
+function Module.CheckBanned(p)
+    local BanInfo = SharedHelper.Data.TempBanned[p.UserId]
+    if BanInfo then      
+        print(os.time() - BanInfo.StartTime .. " seconds has passed sinced banned. " .. p.Name .. " was banned for " .. BanInfo.PublishedLength .. " (" .. BanInfo.RealLength .. ")")
+        if os.time() - BanInfo.StartTime < BanInfo.RealLength then
+            p:Kick("TEMP BANNED (duration: " .. BanInfo.PublishedLength .. "): " .. BanInfo.Reason)
+        else
+            SharedHelper.Data.TempBanned[p.UserId] = nil
+            print(p.Name .. "'s ban was lifted")
+        end
+    end
+end
+
+function Module.CheckpBanned(p)
+    local success, msg = pcall(function()
+        local BanInfo = BanStore:GetAsync(p.UserId)
+        if BanInfo then      
+            print(p.Name .. " tried to join. " .. os.time() - BanInfo.StartTime .. " seconds has passed sinced banned. " .. p.Name .. " was banned for " .. BanInfo.PublishedLength .. " (" .. BanInfo.RealLength .. ")")
+            if os.time() - BanInfo.StartTime < BanInfo.RealLength then
+                p:Kick("PERMANENTLY BANNED (duration: " .. BanInfo.PublishedLength .. "): " .. BanInfo.Reason)
+            else
+                BanStore:SetAsync(p.UserId, false)
+                print(p.Name .. "'s ban was lifted")
+            end
+        end
+    end)
+
+    if not success then
+        warn("Could not check data stores to see if " .. p.Name .. " (" .. p.UserId .. ") was banned.")
+        warn(msg)
+    end
+end
+
+-- // Handlers \\ --
+
+function Module.PlayerAdded(p)
+    SharedHelper.Data[p.Name] = {}
+    Module.CheckSlocked(p)
+    Module.CheckBanned(p)
+    Module.CheckpBanned(p)
+end
+
+function Module.PlayerRemoved(p)
+    SharedHelper.Data[p.Name] = nil
+end
+
+function Module.FireCommand(p, Data)
+    for _,Heading in pairs(Commands:GetChildren()) do
+        for __,Command in pairs(Heading:GetChildren()) do
+            if Command.Name == Data.Command then
+                return require(Command).Run(p, table.unpack(Data.Arguments))
+            end
+        end
+    end
+end
+
+----------------------------------
 
 return Module
--- src/Server/VelocityAdmin/Server/init.server.lua
+++ src/Server/VelocityAdmin/Server/init.server.lua
@@ -1,74 +1,13 @@
 -- // Variables \\ --
 
 local ServerHelper = require(script.Helper)
+
+-- // Defaults \\ --
+
 ServerHelper.SendScripts()
-
-local DataStoreService = game:GetService("DataStoreService")
-local Settings = require(game.ReplicatedStorage.VelocityAdmin.Modules.Settings)
-local SharedHelper = require(game.ReplicatedStorage.VelocityAdmin.Modules.Helper)
-
-local BanStore = DataStoreService:GetDataStore(Settings.Basic.BanScope)
-local Commands = game.ReplicatedStorage.VelocityAdmin.Modules.Commands
-local Remotes = game.ReplicatedStorage.VelocityAdmin.Remotes
 
 -- // Events \\ --
 
-game.Players.PlayerAdded:Connect(function(p)
-    SharedHelper.Data[p.Name] = {}
-
-    -- Checks if slocked
-    local Reason = SharedHelper.Data.ServerLocked
-    if Reason then
-        p:Kick("Server is locked: " .. Reason)
-    end
-
-    -- Checks if tempbanned
-    local BanInfo = SharedHelper.Data.TempBanned[p.UserId]
-    if BanInfo then      
-        print(os.time() - BanInfo.StartTime .. " seconds has passed sinced banned. " .. p.Name .. " was banned for " .. BanInfo.PublishedLength .. " (" .. BanInfo.RealLength .. ")")
-        if os.time() - BanInfo.StartTime < BanInfo.RealLength then
-            p:Kick("TEMP BANNED (duration: " .. BanInfo.PublishedLength .. "): " .. BanInfo.Reason)
-        else
-            SharedHelper.Data.TempBanned[p.UserId] = nil
-            print(p.Name .. "'s ban was lifted")
-        end
-    end
-
-    -- Checks if permanently banned
-    --local success, msg = pcall(function()
-        BanInfo = BanStore:GetAsync(p.UserId)
-        if BanInfo then      
-            print(os.time() - BanInfo.StartTime .. " seconds has passed sinced banned. " .. p.Name .. " was banned for " .. BanInfo.PublishedLength .. " (" .. BanInfo.RealLength .. ")")
-            if os.time() - BanInfo.StartTime < BanInfo.RealLength then
-                p:Kick("PERMANENTLY BANNED (duration: " .. BanInfo.PublishedLength .. "): " .. BanInfo.Reason)
-            else
-                BanStore:SetAsync(p.UserId, false)
-                print(p.Name .. "'s ban was lifted")
-            end
-        end
-    --end)
-
---[[
-
-    if not success then
-        warn("Could not check data stores to see if " .. p.Name .. " (" .. p.UserId .. ") was banned.")
-        warn(msg)
-    end
-
-]]
-
-end)
-
-game.Players.PlayerRemoving:Connect(function(p)
-    SharedHelper.Data[p.Name] = nil
-end)
-
-Remotes.FireCommand.OnServerInvoke = function(p, Data)
-    for _,Heading in pairs(Commands:GetChildren()) do
-        for __,Command in pairs(Heading:GetChildren()) do
-            if Command.Name == Data.Command then
-                return require(Command).Run(p, table.unpack(Data.Arguments))
-            end
-        end
-    end
-end
+game.Players.PlayerAdded:Connect(ServerHelper.PlayerAdded)
+game.Players.PlayerRemoving:Connect(ServerHelper.PlayerRemoved)
+game.ReplicatedStorage.VelocityAdmin.Remotes.FireCommand.OnServerInvoke = ServerHelper.FireCommand
